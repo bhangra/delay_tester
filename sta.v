fddb806d4e917709f674244b0633417a55d94718
bhangra <bhangra@sfc.wide.ad.jp>
2011-07-26 11:28:57 +0900

    26th comit

--- sta.v
+++ sta.v
@@ -38,7 +38,7 @@ module sta(
 	reg [7:0]	mdc_counter;
 
 	always @(posedge clk)
-		if(reset | (mdc_counter == FALL_COUNT)
+		if(reset | (mdc_counter == FALL_COUNT))
 			mdc_counter <= 1;
 		else
 			mdc_counter <= mdc_counter + 1;
@@ -60,9 +60,11 @@ module sta(
 	always @(posedge clk or posedge reset) 
 	begin
 		if(reset)
-			cmd_reg <= 'h0;
+			cmd_reg		<= 'h0;
+			tri_ctrl	<= 'h0;
 		else if((state == RUN)&& mdc_falling)
-			cmd_reg <= {cmd_reg[30:0],1'b0};
+			cmd_reg 	<= {cmd_reg[30:0],1'b0};
+			tri_ctrl	<= {tri_ctrl[30:0],1'b0};
 	end
 
 	assign phy_mdio_out = cmd_reg[31];
